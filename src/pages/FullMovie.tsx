343a633711028dc19cc6e31bcf7e7842ee542356
andrey <andrei200287@gmail.com>
2022-09-29 00:17:08 +0300

    added addToFavourites button on movie's page

--- src/pages/FullMovie.tsx
+++ src/pages/FullMovie.tsx
@@ -1,12 +1,34 @@
-import React from "react";
+import React, {useEffect, useState} from "react";
 import {useParams} from "react-router-dom";
 import {useGetMovieByIdQuery} from "../store/fake.api";
 import '../assets/styles/FullMovie.scss'
+import {useChangeRequestValueMutation, useDeleteFavMovieMutation, useGetRequestValueQuery} from "../store/request.api";
+import {IShortMovie} from "../types/MovieTypes";
+import redHeart from "../assets/icons/heart.svg";
+import whiteHeart from "../assets/icons/whiteHeart.svg";
 
 export const FullMovie: React.FC = () => {
     const {id} = useParams();
-
     const {data: movie, isLoading} = useGetMovieByIdQuery(`${id?.slice(1, id?.length)}`);
+    const [addToFavs] = useChangeRequestValueMutation();
+    const [removeFromFavs] = useDeleteFavMovieMutation();
+    const {data} = useGetRequestValueQuery(undefined);
+    const [isFav, setIsFav] = useState(false);
+    const toggleFavMovie = async() => {
+        const searchedMovie = data && data.find((item: IShortMovie) => item.imdbID === `${id?.slice(1, id?.length)}`);
+        if (searchedMovie === undefined) {
+            movie && await addToFavs({Title: movie.Title, Poster: movie.Poster, imdbID: `${id}`, id: ''});
+            setIsFav(true);
+        } else {
+            await removeFromFavs(searchedMovie.id);
+            setIsFav(false);
+        }
+    }
+
+    useEffect(() => {
+        movie === undefined ? setIsFav(false) : setIsFav(true);
+
+    }, [movie]);
     return (
         <div
             className={'wrapper'}
@@ -46,6 +68,14 @@ export const FullMovie: React.FC = () => {
                     <h3>
                         box office: {movie.BoxOffice}
                     </h3>
+                    <button
+                        onClick={toggleFavMovie}
+                        className={'px-4 py-2 text-center bg-yellow-700 text-white text-xl rounded h-[45px] w-[70px] flex justify-center items-center'}
+                    >
+                        <div>
+                            <img src={isFav ? redHeart : whiteHeart} alt={'favourites'} className={'w-[25px]'}/>
+                        </div>
+                    </button>
                 </div>
             </div>}
             {movie &&
